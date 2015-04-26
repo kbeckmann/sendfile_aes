e8e97bd4a76a7e284d05c756c0e903be847dc460
Konrad Beckmann <konrad.beckmann@gmail.com>
2015-04-26 15:29:44 +0200

    Fixed dependencies in Makefiles. Fixed stuffs in sendfile_aes.c

--- kernel_module/sendfile_aes.c
+++ kernel_module/sendfile_aes.c
@@ -16,8 +16,7 @@ static int message_err = -1;
 static int num_open_files = 0;
 
 struct t_data {
-	size_t aes_key_length;
-	char *aes_key_data;
+	struct T_SENDFILE_AES_SET_KEY *key;
 	int message;
 };
 
@@ -37,32 +36,131 @@ static int file_write(struct file* file, unsigned char* data, size_t size, loff_
 
 static ssize_t device_read(struct file *file, char __user *buffer, size_t len, loff_t *offset)
 {
-	struct t_data* key_data;
+	struct t_data* this;
 
 	if (!(file && file->private_data)) {
 		printk(DEVICE_NAME " unexpected call to read()\n");
 		return simple_read_from_buffer(buffer, len, offset,
-									   &message_err, sizeof(message_err));
+			&message_err, sizeof(message_err));
 	}
 
-	key_data = (struct t_data*)file->private_data;
-	printk(DEVICE_NAME " read(%d)\n", key_data->message);
+	this = (struct t_data*)file->private_data;
+	printk(DEVICE_NAME " read(%d)\n", this->message);
 	return simple_read_from_buffer(buffer, len, offset,
-								   &key_data->message, sizeof(key_data->message));
+		&this->message, sizeof(this->message));
+}
+
+static ssize_t message_set_key(struct t_data* this, const char __user *buff, size_t len)
+{
+	struct T_SENDFILE_AES_SET_KEY set_key;
+	if (len < sizeof(set_key)) {
+		printk(DEVICE_NAME " write(): message too short (1)\n");
+		return -1;
+	}
+
+	if (len > 4096 + sizeof(set_key)) {
+		printk(DEVICE_NAME " write(): message too long\n");
+		return -1;
+	}
+
+	this->key = kmalloc(len, GFP_KERNEL);
+	copy_from_user(this->key, buff, len);
+
+	if (len > this->key->key_length + 31) {
+		printk(DEVICE_NAME " write(): key_length != len; %d != %ld\n",
+			this->key->key_length,
+			len);
+		return -1;
+	}
+
+	{
+		int i;
+		printk(DEVICE_NAME " write(): key: [");
+		for (i = 0; i < this->key->key_length; i++)
+			printk("%02x", this->key->key_data[i]);
+		printk("]\n");
+	}
+	
+	this->message = 0;
+	return 0;
+}
+
+static ssize_t message_sendfile(struct t_data* this, const char __user *buff, size_t len)
+{
+	struct T_SENDFILE_AES_SENDFILE message;
+	char tmp_buf[100];
+	ssize_t n;
+	struct file *file_in;
+	struct file *file_out;
+
+	printk(DEVICE_NAME " enum: %ld, int: %ld, off_t*: %ld, size_t: %ld, "
+		"T_SENDFILE_AES_SENDFILE:%ld\n",
+		sizeof(enum e_message_type),
+		sizeof(int),
+		sizeof(off_t*),
+		sizeof(size_t),
+		sizeof(message));
+
+	if (len < sizeof(message)) {
+		printk(DEVICE_NAME " size mismatch %ld should be %ld\n",
+			   len, sizeof(message));
+		this->message = -1;
+		return -1;
+	}
+
+	copy_from_user(&message, buff, sizeof(message));
+	printk(DEVICE_NAME " message received (%d, %d, %p, %ld)\n",
+		   message.out_fd,
+		   message.in_fd,
+		   message.offset,
+		   message.count);
+
+	if (!message.offset) {
+		this->message = -1;
+		return -1;
+	}
+
+	printk(DEVICE_NAME " message received (*offset=%ld)\n",
+		   *message.offset);
+
+	file_in = fget(message.in_fd);
+	file_out = fget(message.out_fd);
+	while ((n = file_in->f_op->read(file_in, tmp_buf,
+		sizeof(tmp_buf), &file_in->f_pos)) > 0) {
+		loff_t offset = 0;
+
+		// "encrypt"
+		ssize_t i;
+
+		printk(DEVICE_NAME " message_sendfile(): f_op->read()\n");
+		for (i = 0; i < n; i++) {
+			// mostly harmless scrambling
+			if (tmp_buf[i] == 'A') tmp_buf[i] = 'B';
+		}
+
+		// write to out_fd
+		file_write(file_out, tmp_buf, n, &offset);
+		tmp_buf[sizeof(tmp_buf) - 1] = '\0';
+		printk(DEVICE_NAME " message_sendfile(): [%s]\n", tmp_buf);
+	}
+	printk(DEVICE_NAME " n= %d\n", n);
+	this->message = message.count;
+	printk(DEVICE_NAME " message: %d\n", this->message);
+	return this->message;
 }
 
 static ssize_t device_write(struct file *file, const char __user *buff, size_t len, loff_t *off)
 {
-	struct t_data* key_data;
+	struct t_data* this;
 	enum e_message_type message_type;
 
 	if (!(file && file->private_data)) {
 		printk(DEVICE_NAME " write(): invalid stuff\n");
 		return -1;
 	}
+	this = (struct t_data*)file->private_data;
 
 	if (!off) {
-		// Implementation does not handle "off"
 		printk(DEVICE_NAME " write() warning: off is null\n");
 	} else {
 		printk(DEVICE_NAME " write() *off: %lld\n", *off);
@@ -78,7 +176,14 @@ static ssize_t device_write(struct file *file, const char __user *buff, size_t l
 		return -1;
 	}
 
-	key_data = (struct t_data*)file->private_data;
+	if (*off == 0) {
+		// First time write() is called, assume we want to set the key.
+		// This allows us to skip one call to copy_from_user()
+		// TODO: Decide if message_type should be sent or not
+		buff += sizeof(message_type);
+		len -= sizeof(message_type);
+		return message_set_key(this, buff, len);
+	}
 
 	copy_from_user(&message_type, buff, sizeof(message_type));
 	buff += sizeof(message_type);
@@ -87,97 +192,13 @@ static ssize_t device_write(struct file *file, const char __user *buff, size_t l
 	printk(DEVICE_NAME " write(): message_type: %d\n", message_type);
 	switch (message_type) {
 		case MESSAGE_TYPE_SET_KEY:
-		{
-			struct T_SENDFILE_AES_SET_KEY set_key;
-			if (len < sizeof(set_key)) {
-				printk(DEVICE_NAME " write(): message too short (1)\n");
-				return -1;
-			}
-
-			copy_from_user(&set_key, buff, sizeof(set_key));
-			buff += sizeof(set_key);
-			len -= sizeof(set_key);
-
-			if (len < set_key.key_length) {
-				printk(DEVICE_NAME " write(): message too short (2)\n");
-				return -1;
-			}
-
-			key_data->aes_key_length = set_key.key_length;
-			key_data->aes_key_data = kmalloc(key_data->aes_key_length, GFP_KERNEL);
-
-			copy_from_user(key_data->aes_key_data, buff + sizeof(size_t),
-						   key_data->aes_key_length);
-			printk(DEVICE_NAME " copy_from_user copied %ld bytes\n", key_data->aes_key_length);
-			key_data->message = 0;
-			return 0;
-		}
-		break;
+			return message_set_key(this, buff, len);
 		case MESSAGE_TYPE_GET_KEY:
-		break;
+			return 0;
 		case MESSAGE_TYPE_SENDFILE:
-		{
-			struct T_SENDFILE_AES_SENDFILE message;
-
-			printk(DEVICE_NAME " enum: %ld, int: %ld, off_t*: %ld, size_t: %ld, T_SENDFILE_AES_SENDFILE:%ld\n",
-				   sizeof(enum e_message_type),
-				   sizeof(int),
-				   sizeof(off_t*),
-				   sizeof(size_t),
-				   sizeof(message)
-				  );
-
-			if (len < sizeof(message)) {
-				printk(DEVICE_NAME " size mismatch %ld should be %ld\n",
-					   len, sizeof(message));
-				key_data->message = -1;
-				return -1;
-			}
-
-			copy_from_user(&message, buff, sizeof(message));
-			printk(DEVICE_NAME " message received (%d, %d, %p, %ld)\n",
-				   message.out_fd,
-				   message.in_fd,
-				   message.offset,
-				   message.count);
-
-			if (!message.offset) {
-				key_data->message = -1;
-				return -1;
-			}
-
-			printk(DEVICE_NAME " message received (*offset=%ld)\n",
-				   *message.offset);
-
-			{
-				char tmp_buf[100];
-				ssize_t n;
-				struct file *file_in = fget(message.in_fd);
-				struct file *file_out = fget(message.out_fd);
-				while ((n = file_in->f_op->read(file_in, tmp_buf, sizeof(tmp_buf), &file_in->f_pos)) > 0)
-				{
-					loff_t offset = 0;
-
-					// "encrypt"
-					ssize_t i;
-					for (i = 0; i < n; i++)
-					{
-						// mostly harmless scrambling
-						if (tmp_buf[i] == 'A') tmp_buf[i] = 'B';
-					}
-
-					// write to out_fd
-					file_write(file_out, tmp_buf, n, &offset);
-				}
-				key_data->message = message.count;
-			}
-			printk(DEVICE_NAME " message: %d\n", key_data->message);
-			return key_data->message;
-
-		}
-		break;
+			return message_sendfile(this, buff, len);
 		default:
-		printk(DEVICE_NAME " write() invalid message_type\n");
+			printk(DEVICE_NAME " write() invalid message_type\n");
 	}
 
 	return -1;
@@ -190,8 +211,7 @@ static int device_open(struct inode *inode, struct file *file)
 		struct t_data* data = (struct t_data*) kmalloc(sizeof(struct t_data), GFP_KERNEL);
 
 		printk(DEVICE_NAME " private_data: %p\n", file->private_data);
-		data->aes_key_length = 0;
-		data->aes_key_data = 0;
+		data->key = 0;
 		data->message = 0;
 		file->private_data = data;
 
@@ -206,13 +226,13 @@ static int device_release(struct inode *inode, struct file *file)
 	if (file) {
 		printk(DEVICE_NAME " private_data: %p\n", file->private_data);
 		if (file->private_data) {
-			struct t_data* key_data = (struct t_data*)file->private_data;
-			if (key_data->aes_key_data) {
-				printk(DEVICE_NAME " freeing key_data->aes_key_data: %p\n", key_data->aes_key_data);
-				kfree(key_data->aes_key_data);
-				key_data->aes_key_data = 0;
+			struct t_data* this = (struct t_data*)file->private_data;
+			if (this->key) {
+				printk(DEVICE_NAME " freeing this->key: %p\n", this->key);
+				kfree(this->key);
+				this->key = 0;
 			}
-			printk(DEVICE_NAME " freeing key_data\n");
+			printk(DEVICE_NAME " freeing this\n");
 			kfree(file->private_data);
 			file->private_data = 0;
 		}

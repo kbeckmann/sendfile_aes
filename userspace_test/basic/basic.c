3701e5a78bbb2b99bdceed631864174353b04ece
Konrad Beckmann <konrad.beckmann@gmail.com>
2015-05-02 15:48:18 +0200

    Pimped the basic test

--- userspace_test/basic/basic.c
+++ userspace_test/basic/basic.c
@@ -1,21 +1,96 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
 
 #include "sendfile_aes_client.h"
 
-int main()
+static int delay_seconds = 0; // TODO: add argument
+
+void do_delay()
 {
-	char key[] = "1234abcd567";
+	if (delay_seconds > 0)
+		sleep(delay_seconds);
+}
 
-	int handle = sendfile_aes_open(strlen(key), key);
-	sleep(5);
+static void die (const char * format, ...)
+{
+    va_list vargs;
+    va_start (vargs, format);
+    vfprintf (stderr, format, vargs);
+    fprintf (stderr, "\n");
+    exit (1);
+}
 
+int print_help(int argc, char **argv)
+{
+	(void) argc;
+	printf("Usage: %s <in_file> <out_file> [key]\n"
+		   "Note: out_file will be over-written.\n",
+		   argv[0]);
+	return -1;
+}
+
+int do_sendfile(char *filename_out, char *filename_in)
+{
+	int in_fd = open(filename_in, O_RDONLY);
+	if (in_fd < 0) die("Can't open file: %s", filename_in);
+
+	int out_fd = open(filename_out, O_WRONLY | O_CREAT, 0644);
+	if (out_fd < 0) die("Can't open file: %s", filename_in);
+
+	struct stat stat_buf;
+	fstat(in_fd, &stat_buf);
 	off_t offset = 0;
-	sendfile_aes_send(handle, 23, 42, &offset, 256);
-	sleep(5);
+	sendfile(out_fd, in_fd, &offset, stat_buf.st_size);
+	do_delay();
 
-	sendfile_aes_close(handle);
+	close(out_fd);
+	close(in_fd);
 
 	return 0;
+}
+
+int do_sendfile_aes(char *key, char *filename_out, char *filename_in)
+{
+	int handle = sendfile_aes_open(strlen(key), key);
+	do_delay();
+
+	int in_fd = open(filename_in, O_RDONLY);
+	if (in_fd < 0) die("Can't open file: %s", filename_in);
+
+	int out_fd = open(filename_out, O_WRONLY | O_CREAT, 0644);
+	if (out_fd < 0) die("Can't open file: %s", filename_in);
+
+	struct stat stat_buf;
+	fstat(in_fd, &stat_buf);
+	off_t offset = 0;
+	sendfile_aes_send(handle, out_fd, in_fd, &offset, stat_buf.st_size);
+	do_delay();
+
+	sendfile_aes_close(handle);
+	close(out_fd);
+	close(in_fd);
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	char *filename_in = argv[1];
+	char *filename_out = argv[2];
+	char *key = argv[3];
+
+	switch (argc) {
+	case 3:
+		return do_sendfile(filename_out, filename_in);
+	case 4:
+		return do_sendfile_aes(key, filename_out, filename_in);
+	default:
+		return print_help(argc, argv);
+	}
 }

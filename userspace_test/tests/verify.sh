cc12b46e96b4869d251f51ce8730df251ba16063
Konrad Beckmann <konrad.beckmann@gmail.com>
2015-05-10 12:40:31 +0200

    Fixed printout

--- userspace_test/tests/verify.sh
+++ userspace_test/tests/verify.sh
@@ -4,8 +4,9 @@ echo Verifying...
 
 for i in $(ls -1 rand.*.bin) A.bin 100M.bin
 do
+#	echo "  Verifying $i"
 	$SHELL test_basic.sh $i
 	$SHELL test_openssl.sh $i
-	diff $i.enc $i.openssl &> /dev/null || echo "ERROR: $i differ"
+	diff $i.enc $i.openssl &> /dev/null || echo -e "\n    ERROR: $i differ\n"
 done
 
